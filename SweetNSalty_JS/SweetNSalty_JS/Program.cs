974184e6660a3d446ea232b3c2fe48f6cd50cb99
tristyn linde <tristyn.linde@revature.net>
2021-03-15 15:59:10 -0500

    add sweet n salty js version

---
+++ SweetNSalty_JS/SweetNSalty_JS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SweetNSalty_JS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
